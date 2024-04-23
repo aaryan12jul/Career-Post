c4da1b1147ad0d90a85d150f981afa67fdb5a086
WhiteLionGod <aaryan12jul@gmail.com>
2024-04-23 16:37:11 -0400

    Add Editor and Basic Admin Functions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Text, Boolean
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
@@ -12,10 +13,9 @@ from flask_gravatar import Gravatar
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, date
 from functools import wraps
-from admin import Secret, Admin, Premium
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = Secret.FLASK_KEY
+app.secret_key = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 bootstrap = Bootstrap5(app)
 
@@ -29,7 +29,7 @@ def load_user(user_id):
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = Secret.DB_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -55,10 +55,10 @@ class User(db.Model, UserMixin):
     name: Mapped[str] = mapped_column(String(250), nullable=False)
     email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    about_text: Mapped[str] = mapped_column(Text, nullable=False, default=f"Hello, It is nice to Meet You!")
 
     posts: Mapped[list["Post"]] = relationship("Post", back_populates="author")
     comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="author")
-    about: Mapped[list["About"]] = relationship("About", back_populates="author")
 
 class Comment(db.Model):
     __tablename__ = "comments"
@@ -70,13 +70,12 @@ class Comment(db.Model):
     post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("posts.id"))
     post: Mapped["Post"] = relationship("Post", back_populates="comments")
 
-class About(db.Model):
-    __tablename__ = "about_page"
+class Type_User(db.Model):
+    __tablename__ = "typeofuser"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    text: Mapped[str] = mapped_column(Text, nullable=False, default=f"Hello, It is nice to Meet You!")
-
-    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-    author: Mapped["User"] = relationship("User", back_populates="about")
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    admin: Mapped[bool] = mapped_column(Boolean, nullable=False, unique=True)
+    premium: Mapped[bool] = mapped_column(Boolean, nullable=False, unique=True)
 
 with app.app_context():
     db.create_all()
@@ -106,25 +105,27 @@ class CreateAboutForm(FlaskForm):
 def admin_only(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
-        for admin in Admin.ADMINS:
-            try:
-                if current_user.email == admin:
-                    return function(*args, **kwargs)
-            except AttributeError:
-                break
-        return abort(403)
+        user = db.session.execute(db.select(Type_User).where(Type_User.email==current_user.email)).scalar()
+        if current_user.get_id() != '1':
+            if user and user.admin:
+                return function(*args, **kwargs)
+            return abort(403)
+        else:
+            user.admin = True
+            return function(*args, **kwargs)
     return wrapper
 
 def premium_only(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
-        for premium in Premium.PAID:
-            try:
-                if current_user.email == premium:
-                    return function(*args, **kwargs)
-            except AttributeError:
-                break
-        return abort (403)
+        user = db.session.execute(db.select(Type_User).where(Type_User.email==current_user.email)).scalar()
+        if current_user.get_id() != '1':
+            if user and user.premium:
+                return function(*args, **kwargs)
+            return abort(403)
+        else:
+            user.admin = True
+            return function(*args, **kwargs)
     return wrapper
 
 def logged_on(function):
@@ -140,7 +141,7 @@ def logged_on(function):
 @app.route('/')
 def homepage():
     posts = db.session.execute(db.select(Post).order_by(Post.id)).scalars().all()
-    return render_template('index.html', posts=list(reversed(posts)), active0="active", count_target=6, year=year, title="Career Post", logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+    return render_template('index.html', posts=list(reversed(posts)), active0="active", count_target=6, year=year, title="Career Post", logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -159,17 +160,21 @@ def register():
                 password=password
             )
 
-            about_user = About(author=new_user)
+            new_type_user = Type_User(
+                email=email,
+                admin=False,
+                premium=False
+            )
 
             db.session.add(new_user)
-            db.session.add(about_user)
+            db.session.add(new_type_user)
             db.session.commit()
 
             login_user(new_user)
-            return redirect(url_for('homepage'))
+            return redirect(url_for('posts'))
         flash("The Email You Entered Already Exists")
         return redirect('login')
-    return render_template("form.html", form=form, active3="active", year=year, title="Register", logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+    return render_template("form.html", form=form, active3="active", year=year, title="Register", logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -179,26 +184,26 @@ def login():
         user = db.session.execute(db.select(User).where(User.email==email)).scalar()
         if user and check_password_hash(pwhash=user.password, password=form.password.data):
             login_user(user)
-            return redirect(url_for('homepage'))
+            return redirect(url_for('posts'))
         flash("The Email/Password You Entered Is Invalid")
-    return render_template("form.html", form=form, active2="active", year=year, title="Log In", logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+    return render_template("form.html", form=form, active2="active", year=year, title="Log In", logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('homepage'))
+    return redirect(url_for('login'))
 
 @app.route('/posts')
 def posts():
     posts = db.session.execute(db.select(Post).order_by(Post.id)).scalars().all()
-    return render_template('posts.html', posts=list(reversed(posts)), active1="active", count_target=20, year=year, title="Latest Posts", logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS)
+    return render_template('posts.html', posts=list(reversed(posts)), active1="active", count_target=20, year=year, title="Latest Posts", logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all())
 
 @app.route('/view-post/<id>')
 def view_post(id):
     post = db.get_or_404(Post, id)
     if post:
         posts = db.session.execute(db.select(Post).where(Post.author_id==post.author_id)).scalars().all()
-        return render_template('viewer.html', posts=list(reversed(posts)), count_target=3, email=post.author.email, title=post.title, subtitle=post.subtitle, name=post.author.name, text=post.text, image=post.img_url, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+        return render_template('viewer.html', edit_url=url_for('edit_post', email=post.author.email, id=id), id=id, posts=list(reversed(posts)), count_target=3, email=post.author.email, title=post.title, subtitle=post.subtitle, name=post.author.name, text=post.text, image=post.img_url, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
     return redirect(url_for('posts'))
 
 @app.route('/create-post', methods=['GET', 'POST'])
@@ -217,16 +222,89 @@ def create_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("posts"))
-    return render_template("form.html", form=form, year=year, title="Log In", logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+    return render_template("form.html", form=form, year=year, title="Log In", logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
+
+@app.route('/edit-post/<email>/<id>', methods=['GET', 'POST'])
+@logged_on
+def edit_post(email, id):
+    if current_user.email == email:
+        user = db.session.execute(db.select(User).where(User.email==email)).scalar()
+        posts = db.session.execute(db.select(Post).where(Post.author_id==user.id)).scalars().all()
+        post = db.get_or_404(Post, id)
+        form = CreatePostForm(
+            title=post.title,
+            subtitle=post.subtitle,
+            img_url=post.img_url,
+            body=post.text
+            )
+        if form.validate_on_submit():
+            post.title = form.title.data
+            post.subtitle = form.subtitle.data
+            post.img_url = form.img_url.data
+            post.text = form.body.data
+            db.session.commit()
+            return redirect(url_for('view_post', id=id))
+        return render_template('editor.html', form=form, posts=list(reversed(posts)), count_target=3, email=user.email, title=user.name, name=user.name, text=user.about_text, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
+    return abort(403)
+
+@app.route('/delete/<email>/<id>')
+@logged_on
+def delete_post(email, id):
+    if current_user.email == email:
+        post = db.get_or_404(Post, id)
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for('about', email=email))
+    return abort(403)
 
 @app.route('/about/<email>')
 def about(email):
     user = db.session.execute(db.select(User).where(User.email==email)).scalar()
     if user:
-        about = db.session.execute(db.select(About).where(About.author_id==user.id)).scalar()
         posts = db.session.execute(db.select(Post).where(Post.author_id==user.id)).scalars().all()
-        return render_template('viewer.html', posts=list(reversed(posts)), count_target=3, email=user.email, title=user.name, name=user.name, text=about.text, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=Admin.ADMINS, premiums=Premium.PAID)
+        return render_template('viewer.html', edit_url=url_for('edit_about', email=user.email), posts=list(reversed(posts)), count_target=3, email=user.email, title=user.name, name=user.name, text=user.about_text, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
     return abort(403)
+
+@app.route('/edit-about/<email>', methods=['GET', 'POST'])
+@logged_on
+def edit_about(email):
+    if current_user.email == email:
+        user = db.session.execute(db.select(User).where(User.email==email)).scalar()
+        posts = db.session.execute(db.select(Post).where(Post.author_id==user.id)).scalars().all()
+        form = CreateAboutForm(body=user.about_text)
+        if form.validate_on_submit():
+            user.about_text = form.body.data
+            db.session.commit()
+            return redirect(url_for('about', email=email))
+        return render_template('editor.html', form=form, posts=list(reversed(posts)), count_target=3, email=user.email, title=user.name, name=user.name, text=user.about_text, year=year, logged_in=current_user.is_authenticated, user=current_user, admins=db.session.execute(db.select(Type_User).where(Type_User.admin==True)).scalars().all(), premiums=db.session.execute(db.select(Type_User).where(Type_User.premium==True)).scalars().all())
+    return abort(403)
+
+@app.route('/delete/<email>')
+@logged_on
+def delete_account(email):
+    if current_user.email == email:
+        logout_user()
+        user = db.session.execute(db.select(User).where(User.email==email)).scalar()
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for('register'))
+    return abort(403)
+
+@app.route('/make-admin/<email>')
+@admin_only
+def make_admin(email):
+    user = db.session.execute(db.select(Type_User).where(Type_User.email==email)).scalar()
+    user.admin == True
+    db.session.commit()
+    return redirect(url_for('about', email=email))
+
+@app.route('/make-premium/<email>')
+@admin_only
+def make_premium(email):
+    user = db.session.execute(db.select(Type_User).where(Type_User.email==email)).scalar()
+    user.premium == True
+    db.session.commit()
+    return redirect(url_for('about', email=email))
 
 if __name__ == '__main__':
     app.run(debug=True)
